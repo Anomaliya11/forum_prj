8f78ef37a4b430efebc9516288a71cc565a9788f
Katherine Swanson <Katherine@iMac-Katherine-Swanson.local>
2023-03-21 16:33:12 +0300

    less_29

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,14 @@
 from django.db import router
 from django.urls import path, include
 from rest_framework import routers
-from api.views import CheckboxViewSet
 
-router = routers.DefaultRouter()
-router.register('checkbox', CheckboxViewSet)
+from api import views
+# from api.views import CheckboxViewSet
+
+# router = routers.DefaultRouter()
+# router.register('checkbox', CheckboxViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    # path('', include(router.urls)),
+    path('checkbox_list', views.checkbox_list, name='checkbox_list'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,11 +1,20 @@
-from django.shortcuts import render
-
+# from django.shortcuts import render
 from django.forms import CharField
 
 from rest_framework import viewsets
+from api import serializers
 from api.models import Checkbox
 from api.serializers import CheckboxSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
-class CheckboxViewSet(viewsets.ModelViewSet):
-    queryset = Checkbox.objects.all()
-    serializer_class = CheckboxSerializer
+# class CheckboxViewSet(viewsets.ModelViewSet):
+#     queryset = Checkbox.objects.all()
+#     serializer_class = CheckboxSerializer
+
+@api_view(['CET'])
+def checkbox_list(req):
+    checkboxes = Checkbox.objects.all()
+    serializer = CheckboxSerializer(checkboxes, many=True)
+    return Response(serializer.data)
